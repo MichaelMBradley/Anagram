3986d82524d7b66e405a404765028d2f6f8f2f06
Michael Bradley <michaelmbrad@gmail.com>
2021-07-31 17:08:20 -0400

    Create files

---
+++ pureanagram.py
@@ -0,0 +1,12 @@
+from primes import get_n_primes
+from words import get_all
+
+primes = get_n_primes(26)
+words = get_all()
+
+
+def encode_word(word: str) -> int:
+    e = 1
+    for c in word:
+        pass
+    return e
