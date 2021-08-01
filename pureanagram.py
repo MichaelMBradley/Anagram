b7ce809f56f233e1301986a897013968b1c24412
Michael Bradley <michaelmbrad@gmail.com>
2021-08-01 09:51:32 -0400

    Encode words

--- pureanagram.py
+++ pureanagram.py
@@ -5,8 +5,18 @@ primes = get_n_primes(26)
 words = get_all()
 
 
-def encode_word(word: str) -> int:
-    e = 1
-    for c in word:
-        pass
-    return e
+def encode_word(string: str) -> int:
+    product = 1
+    for char in string:
+        product *= primes[ord(char.lower()) - ord('a')]
+    return product
+
+
+if __name__ == "__main__":
+    print("Enter a word to get its encoding.\n'*' ends execution.")
+    while True:
+        word = input("> ")
+        if word == '*':
+            break
+        else:
+            print(encode_word(word))
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import unittest
 from unittest import TestCase
 
 from primes import get_n_primes
+from pureanagram import encode_word
 from words import get_all
 
 
@@ -13,6 +14,12 @@ class Test(TestCase):
 
     def test_get_all(self):
         self.assertEqual(get_all()[0], 'a')
+
+    def test_encode_word(self):
+        self.assertEqual(encode_word(""), 1)
+        self.assertEqual(encode_word("a"), 2)
+        self.assertEqual(encode_word("abc"), 30)
+        self.assertEqual(encode_word("aBC"), 30)
 
 
 if __name__ == "__main__":
