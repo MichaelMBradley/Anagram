cb4a0f7f07f3c69e7858426d50cb6cb2f1f9c7a6
Michael Bradley <michaelmbrad@gmail.com>
2021-08-02 09:35:16 -0400

    Swap to tabs.
    https://www.reddit.com/r/javascript/comments/c8drjo/nobody_talks_about_the_real_reason_to_use_tabs/

--- test.py
+++ test.py
@@ -7,20 +7,20 @@ from words import get_all
 
 
 class Test(TestCase):
-    def test_get_n_primes(self):
-        self.assertEqual(get_n_primes(0), [])
-        self.assertEqual(get_n_primes(1), [2])
-        self.assertEqual(get_n_primes(2), [2, 3])
+	def test_get_n_primes(self):
+		self.assertEqual(get_n_primes(0), [])
+		self.assertEqual(get_n_primes(1), [2])
+		self.assertEqual(get_n_primes(2), [2, 3])
 
-    def test_get_all(self):
-        self.assertEqual(get_all()[0], 'a')
+	def test_get_all(self):
+		self.assertEqual(get_all()[0], 'a')
 
-    def test_encode_word(self):
-        self.assertEqual(encode_word(""), 1)
-        self.assertEqual(encode_word("a"), 2)
-        self.assertEqual(encode_word("abc"), 30)
-        self.assertEqual(encode_word("aBC"), 30)
+	def test_encode_word(self):
+		self.assertEqual(encode_word(""), 1)
+		self.assertEqual(encode_word("a"), 2)
+		self.assertEqual(encode_word("abc"), 30)
+		self.assertEqual(encode_word("aBC"), 30)
 
 
 if __name__ == "__main__":
-    unittest.main()
+	unittest.main()
--- words.py
+++ words.py
@@ -1,97 +1,97 @@
 class InvalidCharacterException(Exception):
-    """
-    Exception raised if a character is not in a-z or A-Z.
+	"""
+	Exception raised if a character is not in a-z or A-Z.
 
-    Attributes
-    ----------
-    char: str
-        The invalid character.
-    message: str
-        A formatted error message.
-    word: str
-        The string containing the invalid character.
-    """
-    def __init__(self, char: str, word: str):
-        """
-        Initializes the exception with relevant information.
+	Attributes
+	----------
+	char: str
+		The invalid character.
+	message: str
+		A formatted error message.
+	word: str
+		The string containing the invalid character.
+	"""
+	def __init__(self, char: str, word: str):
+		"""
+		Initializes the exception with relevant information.
 
-        Parameters
-        ----------
-        char: str
-            The invalid character.
-        word: str
-            The string containing the invalid character.
-        """
-        self.character = char
-        self.word = word
-        self.message = f"Character {self.character} is not in (a-z) or (A-Z)."
-        super().__init__(self.message)
+		Parameters
+		----------
+		char: str
+			The invalid character.
+		word: str
+			The string containing the invalid character.
+		"""
+		self.character = char
+		self.word = word
+		self.message = f"Character {self.character} is not in (a-z) or (A-Z)."
+		super().__init__(self.message)
 
-    def __str__(self):
-        """
-        Better formats the exception message.
+	def __str__(self):
+		"""
+		Better formats the exception message.
 
-        Returns
-        -------
-        str
-            The formatted exception message, containing the invalid word and the invalid character.
-        """
-        return f"{self.word} -> {self.message}"
+		Returns
+		-------
+		str
+			The formatted exception message, containing the invalid word and the invalid character.
+		"""
+		return f"{self.word} -> {self.message}"
 
 
 class NoAnagramsException(Exception):
-    """
-    Exception raised if there is no anagram of a string.
+	"""
+	Exception raised if there is no anagram of a string.
 
-    Attributes
-    ----------
-    message: str
-        A formatted error message.
-    word: str
-        Invalid character.
-    """
-    def __init__(self, word: str):
-        """
-        Initializes the exception with relevant information.
+	Attributes
+	----------
+	message: str
+		A formatted error message.
+	word: str
+		Invalid character.
+	"""
+	def __init__(self, word: str):
+		"""
+		Initializes the exception with relevant information.
 
-        Parameters
-        ----------
-        word: str
-            The string with no anagrams.
-        """
-        self.word = word
-        self.message = f"{self.word} has no known anagrams."
-        super().__init__(self.message)
+		Parameters
+		----------
+		word: str
+			The string with no anagrams.
+		"""
+		self.word = word
+		self.message = f"{self.word} has no known anagrams."
+		super().__init__(self.message)
 
-    def __str__(self):
-        """
-        Better formats the exception message.
-        
-        Returns
-        -------
-        str
-            The formatted exception message containing the word with no anagrams.
-        """
-        return f"{self.word} -> {self.message}"
+	def __str__(self):
+		"""
+		Better formats the exception message.
+
+		Returns
+		-------
+		str
+			The formatted exception message containing the word with no anagrams.
+		"""
+		return f"{self.word} -> {self.message}"
 
 
 def get_all(file: str = "words_alpha") -> list[str]:
-    """
-    Retrieves every word in the given file.
+	"""
+	Retrieves every word in the given file.
 
-    Parameters
-    ----------
-    file : str (optional)
-        The file to read the words from, assuming it is located in 'words/' and is a '.txt' file.
+	Parameters
+	----------
+	file : str (optional)
+		The file to read the words from, assuming it is located in 'words/' and is a '.txt' file.
 
-    Returns
-    -------
-    list[str]
-        Every word in the file.
-    """
-    with open(f"words/{file}.txt") as words:
-        return words.read().split()
+	Returns
+	-------
+	list[str]
+		Every word in the file.
+	"""
+	with open(f"words/{file}.txt") as words:
+		return words.read().split()
 
 
 if __name__ == "__main__":
-    print(get_all())
+	print(get_all())
